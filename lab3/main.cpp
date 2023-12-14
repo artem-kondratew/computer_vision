dc184efb0952d63404650e9bb06bb931b646ace1
artem-kondratew <artemkondratev5@gmail.com>
2023-12-14 23:56:42 +0300

    task3 completed

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,14 +4,20 @@
 #include <opencv4/opencv2/imgproc.hpp>
 #include <opencv4/opencv2/highgui.hpp>
 
-
-cv::Scalar MAGENTA(255, 0, 255);
-cv::Scalar RED(0, 0, 255);
-cv::Scalar BLACK(0, 0, 0);
-cv::Scalar WHITE(255, 255, 255);
+#include "hsv_picker.hpp"
 
 
-std::vector<std::vector<cv::Point>> findContours(cv::Mat gray, int thr) {
+const cv::Scalar MAGENTA(255, 0, 255);
+
+const cv::Scalar BLUE(255, 0, 0);
+const cv::Scalar GREEN(0, 255, 0);
+const cv::Scalar RED(0, 0, 255);
+
+const cv::Scalar BLACK(0, 0, 0);
+const cv::Scalar WHITE(255, 255, 255);
+
+
+std::vector<std::vector<cv::Point>> findContours(const cv::Mat gray, const int thr) {
     cv::Mat thresh;
     cv::threshold(gray, thresh, thr, 255, cv::THRESH_BINARY);
 
@@ -80,7 +86,7 @@ void task1() {
 }
 
 
-cv::Mat findRed(cv::Mat src) {
+cv::Mat findRed(const cv::Mat src) {
     cv::Scalar low_red(0, 100, 100);
     cv::Scalar high_red(20, 255, 255);
 
@@ -94,7 +100,7 @@ cv::Mat findRed(cv::Mat src) {
 }
 
 
-int findMaxContour(std::vector<std::vector<cv::Point>> contours) {
+int findMaxContour(const std::vector<std::vector<cv::Point>> contours) {
     double p = 0;
     int idx = 0;
 
@@ -110,7 +116,7 @@ int findMaxContour(std::vector<std::vector<cv::Point>> contours) {
 }
 
 
-cv::Mat findMotor(cv::Mat src) {
+cv::Mat findMotor(const cv::Mat src) {
     cv::Mat red = findRed(src);
     
     cv::Mat dilate;
@@ -154,10 +160,175 @@ void task2() {
 }
 
 
+cv::Point findLamp(cv::Mat src) {
+    cv::Mat gray;
+    cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+
+    int thr = 250;
+
+    std::vector<std::vector<cv::Point>> contours = findContours(gray, thr);
+
+    cv::drawContours(src, contours, -1, MAGENTA, 2, 2);
+
+    cv::Moments m = cv::moments(contours[0]);
+    int x = m.m10 / m.m00;
+    int y = m.m01 / m.m00;
+    
+    return cv::Point(x, y);
+}
+
+
+cv::Mat morph(const cv::Mat src) {
+    cv::Mat morph;
+    cv::Mat erode_kernel = cv::getStructuringElement(cv::MORPH_ERODE, {3, 3});
+    cv::Mat dilate_kernel = cv::getStructuringElement(cv::MORPH_ERODE, {3, 3});
+
+    cv::erode(src, morph, erode_kernel);
+    cv::dilate(morph, morph, dilate_kernel);
+
+    return morph;
+}
+
+
+std::vector<std::vector<cv::Point>> eraseSmallContours(std::vector<std::vector<cv::Point>> contours, int area) {
+    for (int i = 0; i < contours.size(); i++) {
+        if (cv::contourArea(contours[i]) < area) {
+            contours.erase(contours.begin() + i);
+            i--;
+        }
+    }
+
+    return contours;
+}
+
+
+std::vector<cv::Point> findCenters(std::vector<std::vector<cv::Point>> contours) {
+    std::vector<cv::Point> centers;
+
+    for (int i = 0; i < contours.size(); i++) {
+        cv::Moments m = cv::moments(contours[i]);
+        int x = m.m10 / m.m00;
+        int y = m.m01 / m.m00;
+        centers.push_back(cv::Point(x, y));
+    }
+
+    return centers;
+}
+
+
+std::vector<float> findDistances(std::vector<cv::Point> centers, cv::Point lamp_center) {
+    std::vector<float> dist;
+
+    for (int i = 0; i < centers.size(); i++) {
+        float d = cv::norm(centers[i] - lamp_center);
+        dist.push_back(d);
+    }
+
+    return dist;
+}
+
+
+cv::Point findMinDist(std::vector<std::vector<cv::Point>> contours, cv::Point lamp_center) {
+
+    std::vector<cv::Point> centers = findCenters(contours);
+    std::vector<float> dist = findDistances(centers, lamp_center);
+
+    int idx = -1;
+    float min_dist = FLT_MAX;
+
+    for (int i = 0; i < dist.size(); i++) {
+        if (dist[i] < min_dist && dist[i] > 50) {
+            idx = i;
+            min_dist = dist[i];
+        }
+        std::cout << dist.size() << " dist " << dist[i] << std::endl;
+    }
+
+    if (idx == -1) {
+        std::cout << "NOPE" << std::endl;
+    }
+
+    std::cout << idx << std::endl;
+
+    return centers[idx];
+}
+
+
+void task3() {
+    cv::Mat src = cv::imread("/home/user/Projects/computer_vision/lab3/src3/img0.jpg");
+
+    cv::Mat hsv;
+    cv::cvtColor(src, hsv, cv::COLOR_BGR2HSV);
+
+    cv::Scalar red_lowerb(0, 50, 50);
+    cv::Scalar red_upperb(10, 255, 255);
+
+    cv::Scalar red_lowerb_end(170, 50, 50);
+    cv::Scalar red_upperb_end(179, 255, 255);
+
+    cv::Scalar green_lowerb(70, 50, 50);
+    cv::Scalar green_upperb(80, 255, 255);
+
+    cv::Scalar blue_lowerb(90, 50, 50);
+    cv::Scalar blue_upperb(130, 255, 255);
+
+    cv::Mat red, red_0, red_1, green, blue;
+
+    cv::inRange(hsv, red_lowerb, red_upperb, red_0);
+    cv::inRange(hsv, red_lowerb_end, red_upperb_end, red_1);
+    cv::bitwise_or(red_0, red_1, red);
+    cv::inRange(hsv, green_lowerb, green_upperb, green);
+    cv::inRange(hsv, blue_lowerb, blue_upperb, blue);
+
+    std::vector<std::vector<cv::Point>> red_cont, green_cont, blue_cont;
+    cv::findContours(red, red_cont, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    cv::findContours(green, green_cont, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    cv::findContours(blue, blue_cont, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+
+    red_cont = eraseSmallContours(red_cont, 100);
+    green_cont = eraseSmallContours(green_cont, 200);
+    blue_cont = eraseSmallContours(blue_cont, 100);
+
+    cv::drawContours(src, red_cont, -1, RED, 1);
+    cv::drawContours(src, green_cont, -1, GREEN, 1);
+    cv::drawContours(src, blue_cont, -1, BLUE, 1);
+
+    cv::Point lamp_center = findLamp(src);
+
+    // cv::imshow("red", red);
+    // cv::imshow("blue", blue);
+    // cv::imshow("green", green);
+
+    cv::Point red_center = findMinDist(red_cont, lamp_center);
+    cv::Point green_center = findMinDist(green_cont, lamp_center);
+    cv::Point blue_center = findMinDist(blue_cont, lamp_center);
+
+    cv::line(src, lamp_center, red_center, MAGENTA, 2);
+    cv::line(src, lamp_center, green_center, MAGENTA, 2);
+    cv::line(src, lamp_center, blue_center, MAGENTA, 2);
+
+    cv::circle(src, lamp_center, 4, MAGENTA, 2);
+    cv::circle(src, red_center, 4, MAGENTA, 2);
+    cv::circle(src, green_center, 4, MAGENTA, 2);
+    cv::circle(src, blue_center, 4, MAGENTA, 2);
+
+    cv::imshow("result", src);
+
+    cv::waitKey(0);
+}
+
+
+void task4() {
+    
+}
+
+
 int main() {
 
     // task1();
-    task2();
+    // task2();
+    // task3();
+    task4();
 
     return 0;
 }
